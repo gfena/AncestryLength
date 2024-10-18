6559a863d630adf02f63b5395f0ade69b387c926
gfena <106163218+gfena@users.noreply.github.com>
2024-10-18 11:50:42 +0200

    Delete test_loop.sh

--- test_loop.sh
+++
@@ -1,8 +0,0 @@
-
-input="/home/Desktop/Giacomo/LAI/inputbychr/"
-
-for chr in {1..22}
-do
-ruby measure_ancestry_lengthv4.7.rb -i $input/chm_$chr.csv -o $input/Roma.chr$chr.ancestrylength.csv --MB --nohap
-done
-
